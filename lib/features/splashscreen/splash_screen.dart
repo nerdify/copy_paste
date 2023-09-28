63740f34087e4ace0e7568ede3d0a7d34961d47a
Manuel Duarte  <carlosd99m@gmail.com>
2023-09-28 16:36:07 -0600

    feat: enabled to use cupertino widgets

---
+++ lib/features/splashscreen/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      child: Center(
+        child: Text('Copy with Me App'),
+      ),
+    );
+  }
+}
