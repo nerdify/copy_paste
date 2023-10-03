29f2a157f7b745f25f0b4505683b24c5c2e7ec1b
Manuel Duarte  <carlosd99m@gmail.com>
2023-10-03 15:37:58 -0600

    create routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,6 @@
 import 'package:copy_paste/config/app_routes.dart';
 import 'package:copy_paste/features/auth/presentation/bloc/cubit/auth_cubit.dart';
-import 'package:copy_paste/features/list_notes/presentation/list_notes_screen.dart';
-import 'package:copy_paste/features/splashscreen/intro_screen.dart';
-import 'package:copy_paste/features/splashscreen/splash_screen.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,41 +10,32 @@ final _navigatorKey = GlobalKey<NavigatorState>();
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static Widget create() {
-    return BlocListener<AuthCubit, AuthState>(
-      listener: ((context, state) {
-        if (state is AuthSignedOut) {
-          _navigatorKey.currentState?.pushNamedAndRemoveUntil(
-            IntroScreen.routeName,
-            (route) => false,
-          );
-        } else if (state is AuthSignedIn) {
-          _navigatorKey.currentState?.pushNamedAndRemoveUntil(
-            ListNotesScreen.routeName,
-            (route) => false,
-          );
-        }
-      }),
-      child: const MyApp(),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      navigatorKey: _navigatorKey,
-      title: 'Copy with Me App',
-      onGenerateRoute: AppRoute.onGenerateRoute,
-      initialRoute: SplashScreen.routeName,
-      theme: CupertinoThemeData(
-        brightness: Brightness.light,
-        primaryColor: CupertinoColors.systemPurple,
-        scaffoldBackgroundColor: CupertinoColors.white,
-        textTheme: CupertinoTextThemeData(
-          textStyle: GoogleFonts.redHatDisplay()
-              .copyWith(color: CupertinoColors.black),
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state == AuthState.signedOut) {
+          _navigatorKey.currentState
+              ?.pushNamedAndRemoveUntil(Routes.intro, (r) => false);
+        } else if (state == AuthState.signedIn) {
+          _navigatorKey.currentState
+              ?.pushNamedAndRemoveUntil(Routes.home, (r) => false);
+        }
+      },
+      child: CupertinoApp(
+        navigatorKey: _navigatorKey,
+        title: 'Copy with Me App',
+        onGenerateRoute: Routes.routes,
+        theme: CupertinoThemeData(
+          brightness: Brightness.light,
+          primaryColor: CupertinoColors.systemPurple,
+          scaffoldBackgroundColor: CupertinoColors.white,
+          textTheme: CupertinoTextThemeData(
+            textStyle: GoogleFonts.redHatDisplay()
+                .copyWith(color: CupertinoColors.black),
+          ),
+          applyThemeToAll: true,
         ),
-        applyThemeToAll: true,
       ),
     );
   }
--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,31 +1,33 @@
+import 'package:copy_paste/features/list_notes/presentation/edit_note_screen.dart';
 import 'package:copy_paste/features/list_notes/presentation/list_notes_screen.dart';
 import 'package:copy_paste/features/splashscreen/intro_screen.dart';
 import 'package:copy_paste/features/splashscreen/splash_screen.dart';
+
 import 'package:flutter/cupertino.dart';
 
-class AppRoute {
-  static Route onGenerateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/':
-        return ListNotesScreen.route();
-      case SplashScreen.routeName:
-        return SplashScreen.route();
-      case IntroScreen.routeName:
-        return IntroScreen.route();
+class Routes {
+  static const splash = '/';
+  static const intro = '/intro';
+  static const home = '/home';
+  static const editNote = '/editNote';
 
-      default:
-        return _errorRoute();
+  static Route routes(RouteSettings settings) {
+    // Helper nested function.
+    CupertinoPageRoute buildRoute(Widget widget) {
+      return CupertinoPageRoute(builder: (_) => widget, settings: settings);
     }
-  }
 
-  static Route _errorRoute() {
-    return CupertinoPageRoute(
-      settings: const RouteSettings(name: '/error'),
-      builder: (_) => const CupertinoPageScaffold(
-        child: Center(
-          child: Text('Error'),
-        ),
-      ),
-    );
+    switch (settings.name) {
+      case splash:
+        return buildRoute(const SplashScreen());
+      case intro:
+        return buildRoute(const IntroScreen());
+      case home:
+        return buildRoute(const ListNotesScreen());
+      case editNote:
+        return buildRoute(const EditNoteScreen());
+      default:
+        throw Exception('Route does not exists');
+    }
   }
 }
